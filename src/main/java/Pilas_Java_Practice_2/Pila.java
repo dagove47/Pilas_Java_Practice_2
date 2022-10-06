44d88a4110f9f454e8815ad2fc796998a14622c0
David Gomez <dagove47@gmail.com>
2022-10-06 01:49:44 -0600

    Basic structure
    Classes added

---
+++ src/main/java/Pilas_Java_Practice_2/Pila.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pilas_Java_Practice_2;
+
+/**
+ *
+ * @author David Gomez, Gabriel Brilla, Gerald Blanco, Alexander Torres
+ */
+public class Pila {
+    
+}
